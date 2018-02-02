3853d9c36588adc10814e0bada4e4beec6f03820
Majzer <bence@mozer.hu>
2018-02-02 11:04:15 +0100

    Alapok mesterből megvannak

---
+++ core/src/com/csanysoft/donto/DemoLoading/LoadingScreen.java
@@ -0,0 +1,47 @@
+package com.csanysoft.donto.DemoLoading;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.csanysoft.donto.Donto;
+import com.csanysoft.donto.Donto;
+import com.csanysoft.donto.GlobalClasses.*;
+import com.csanysoft.donto.MyBaseClasses.Scene2D.MyScreen;
+
+
+public class LoadingScreen extends MyScreen {
+
+
+    public LoadingScreen(Donto game) {
+		super(game);
+    }
+	BitmapFont bitmapFont = new BitmapFont();
+
+    @Override
+	public void show() {
+	    Assets.manager.finishLoading();
+		Assets.load();
+	}
+
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+
+		spriteBatch.begin();
+		bitmapFont.draw(spriteBatch,"Loading: " + Assets.manager.getLoadedAssets() + "/" + (Assets.manager.getQueuedAssets()+ Assets.manager.getLoadedAssets()) + " (" + ((int)(Assets.manager.getProgress()*100f)) + "%)", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+		spriteBatch.end();
+		if (Assets.manager.update()) {
+			Assets.afterLoaded();
+		}
+	}
+
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void init() {
+		setBackGroundColor(0f, 0f, 0f);
+	}
+}
--- core/src/com/csanysoft/donto/Donto.java
+++ core/src/com/csanysoft/donto/Donto.java
@@ -5,30 +5,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.csanysoft.donto.DemoLoading.LoadingScreen;
+import com.csanysoft.donto.GlobalClasses.Assets;
+import com.csanysoft.donto.MyBaseClasses.Game.MyGame;
 
-public class Donto extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class Donto extends MyGame {
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		System.out.println("uisadfdhwei");
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void create() {
+		Gdx.input.setCatchBackKey(true);
+		Assets.prepare();
+		setScreen(new LoadingScreen(this));
 	}
 }
