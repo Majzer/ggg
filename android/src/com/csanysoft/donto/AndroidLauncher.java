36dca7a6739bbbea68661536e452dc735ae372f1
Majzer <bence@mozer.hu>
2018-02-02 10:49:07 +0100

    Initial commit

---
+++ android/src/com/csanysoft/donto/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.csanysoft.donto;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.csanysoft.donto.Donto;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Donto(), config);
+	}
+}
---
+++ desktop/src/com/csanysoft/donto/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.csanysoft.donto.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.csanysoft.donto.Donto;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Donto(), config);
+	}
+}
