834fde9b2185ae720250435ad4ee7de92f663afc
krinistof <kristof0116@gmail.com>
2018-02-02 11:30:46 +0100

    sétál jobbre

--- core/src/com/csanysoft/donto/AndroidActor.java
+++ core/src/com/csanysoft/donto/AndroidActor.java
@@ -8,7 +8,24 @@ import com.csanysoft.donto.MyBaseClasses.Scene2D.OneSpriteAnimatedActor;
  */
 
 public class AndroidActor extends OneSpriteAnimatedActor {
+
+    private int speed = 4;
+
     public AndroidActor(TextureAtlas textureAtlas) {
         super(textureAtlas);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        setX(getX() + speed);
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
     }
 }
